4cb7d6979a1eb5a2d00a32c5c7fdea202f111da0
ibrahim75 <ibrahimbiplob75@gmail.com>
2023-12-02 00:01:25 +0600

    Menu retive

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const port=process.env.PORT || 5000;
 
 
 // middleware
-app.get(cors());
-app.get(express.json());
+app.use(cors({
+    origin:["http://localhost:5173"],
+    credentials:true,
+}));
+app.use(express.json());
 
 //RestaurantDB
 //menu
